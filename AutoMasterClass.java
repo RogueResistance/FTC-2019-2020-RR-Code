b7fe76ec9e51c5fe3fda9d814788b01805c57f9d
RogueResistance <53359861+RogueResistance@users.noreply.github.com>
2019-11-10 19:15:10 -0600

    Update AutoMasterClass.java

--- AutoMasterClass.java
+++ AutoMasterClass.java
@@ -194,10 +194,9 @@ public abstract class AutoMasterClass extends LinearOpMode {
         for (double currPower = 0.00001; currPower <= power; x++, currPower = Math.pow(currPower, 1.0 / x)) {
             correction(currPower, targetHeading, direction, false);
         }
-//         for (double x = 1, currPower = 0.01; currPower <= power; x++, currPower = 1.0/(x-6) + (7.0/6)) {
+//         for (double currPower = power, x = 1; currPower >= 0; x++, currPower = 0.006*Math.exp(x)) {
 //             correction(currPower, targetHeading, direction, false);
 //         }
-
     }
 
     public void stopStrafe(double power, int targetHeading, String direction, double inches)throws InterruptedException {
@@ -205,7 +204,7 @@ public abstract class AutoMasterClass extends LinearOpMode {
         for(double currPower = 0.0001; currPower < power; x++,power = Math.pow(power, x)) {
             correction(power, targetHeading, direction, false);
         }
-//         for(double currPower = power, x = 1; currPower >= 0; x++, currPower = 0.006*Math.exp(x)) {
+//         for(double x = 1, currPower = 0.01; currPower <= power; x++, currPower = 1.0/(x-6) + (7.0/6)) {
 //             correction(power, targetHeading, direction, false);
 //         }
     }
